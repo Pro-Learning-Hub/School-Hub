e9fe4b05843a1510eba5783ec5ee2f6862011780
loay al-said <l.programming555@gmail.com>
2025-10-24 21:51:36 +0200

    Slightly refactor the Lectures compontn to encapsulare logic and UI

--- front-end/src/components/Lectures/Lectures.jsx
+++ front-end/src/components/Lectures/Lectures.jsx
@@ -1,17 +1,19 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import {
   selectcourseSectionsJS,
   selectLecturesIsLoading,
 } from '../../redux/selectors/lecturesSelectors';
+import './Lectures.css';
 import { getCourseLectures } from '../../redux/actions/lecturesThunks';
 import { Link } from 'react-router-dom';
 import { selectCourseId } from '../../redux/selectors/uiSelectors';
 import { useJoinRoom } from '../../hooks/socketConnectionHooks';
 import useSyncSections from '../../hooks/syncSectionsHook';
-import SearchInput from './SearchInput';
+import SearchInput from '../sharedComponents/SearchInput';
 import SectionsList from './SectionsList';
-
+import useSearchState from '../../hooks/useSearchState';
+import LecturesSearchList from './LecturesSearchList';
 
 export default function Lectures() {
   const isLoading = useSelector(selectLecturesIsLoading);
@@ -19,6 +21,8 @@ export default function Lectures() {
   const userRole = useSelector((state) => state.ui.getIn(['user', 'role']));
   const courseId = useSelector(selectCourseId);
   const dispatch = useDispatch();
+
+  const searchState = useSearchState();
 
   useEffect(() => {
     dispatch(getCourseLectures());
@@ -39,11 +43,23 @@ export default function Lectures() {
             </p>
 
             {/* Search Component */}
-            <SearchInput />
-          </div>
+            <SearchInput 
+              placeholder="Search lectures, descriptions, and tags..."
+              searchQuery={searchState.searchQuery} 
+              setSearchQuery={searchState.setSearchQuery} 
+            />
 
-          {/* Sections List Component */}
-          <SectionsList sections={sections} isLoading={isLoading} />
+          </div>
+          {searchState.showSearchList ? (
+            <LecturesSearchList
+              results={searchState.searchResults}
+              loading={searchState.searchLoading}
+              error={searchState.searchError}
+              query={searchState.debouncedQuery}
+            />
+          ) : (
+            <SectionsList sections={sections} isLoading={isLoading} />
+          )}
         </div>
       </div>
 
