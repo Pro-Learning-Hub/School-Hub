5b4fff71c82138d67d2e20aeb4b9d234b159efc3
loay al-said <l.programming555@gmail.com>
2025-10-24 22:20:57 +0200

    Transform the utils Hooks into TS; use(async, fetch, debounceValue)

--- front-end/src/hooks/useDebounceValue.hook.ts
+++ front-end/src/hooks/useDebounceValue.hook.ts
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 
-export default function useDebounceValue(value, delay = 500) {
-  const [debounced, setDebounced] = useState(value);
+export default function useDebounceValue<T>(value: T, delay: number = 500): T {
+  const [debounced, setDebounced] = useState<T>(value);
 
   useEffect(() => {
     const handler = setTimeout(() => setDebounced(value), delay);
