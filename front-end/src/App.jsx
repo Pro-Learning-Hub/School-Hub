c06811feae307d97fee57112a30c2fecb3ce4492
loay al-said <l.programming555@gmail.com>
2025-10-27 14:07:22 +0200

    Lazy load routes components

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import { useSelector } from 'react-redux';
 import { Toaster } from 'react-hot-toast';
 import { Routes, Route, Navigate, Outlet} from 'react-router-dom';
@@ -8,17 +8,19 @@ import './components/Login/css/main.css';
 import './components/Register/css/util.css';
 import './components/Register/css/main.css';
 import Spinner from './components/utilityComponents/Spinner';
-import Lectures from './components/Lectures/Lectures';
-import Announcements from './components/Announcements/Announcements'
-import GeneralDiscussion from './components/GeneralDiscussion/GeneralDiscussion';
-import Lecture from './components/Lecture/Lecture';
-import Replies from './components/Replies/Replies';
-import Login from './components/Login/Login';
-import Register from './components/Register/Register';
 import Sidebar from './components/Sidebar/sidebar';
-import CreateNewLecture from './components/CreateLectureForm/CreateLectureForm';
-import EditLectureForm from './components/EditLectureForm/EditLectureForm';
 import useConnectSocket from './hooks/socketConnectionHooks';
+
+// Lazy-loaded route components for code splitting
+const Lectures = React.lazy(() => import('./components/Lectures/Lectures'));
+const Announcements = React.lazy(() => import('./components/Announcements/Announcements'));
+const GeneralDiscussion = React.lazy(() => import('./components/GeneralDiscussion/GeneralDiscussion'));
+const Lecture = React.lazy(() => import('./components/Lecture/Lecture'));
+const Replies = React.lazy(() => import('./components/Replies/Replies'));
+const Login = React.lazy(() => import('./components/Login/Login'));
+const Register = React.lazy(() => import('./components/Register/Register'));
+const CreateNewLecture = React.lazy(() => import('./components/CreateLectureForm/CreateLectureForm'));
+const EditLectureForm = React.lazy(() => import('./components/EditLectureForm/EditLectureForm'));
 
 function ProtectedLayout() {
   const isLoggedIn = useSelector((state) => state.ui.get('isLoggedIn'));
@@ -30,6 +32,21 @@ function ProtectedLayout() {
   }
 }
 
+// Loading fallback component for code splitting
+function RouteLoadingSpinner() {
+  return (
+    <div style={{
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      height: '200px',
+      width: '100%'
+    }}>
+      <Spinner />
+    </div>
+  );
+}
+
 function App() {
   const isLoading = useSelector((state) => state.ui.get('isLoading'));
 
@@ -39,21 +56,23 @@ function App() {
     <div className="APP">
       {isLoading && <Spinner />}
       <Sidebar />
-      <Routes>
-        <Route path='/login' element={<Login />} />
-        <Route path='/Register' element={<Register />} />
-        <Route path="/" element={<ProtectedLayout />}>
-          <Route index element={<Lectures />} />
-          <Route path="/lectures" element={<Lectures />} />
-          <Route path="/lectures/:lectureId" element={<Lecture />} />
-          <Route path="/lectures/new" element={<CreateNewLecture />} />
-          <Route path="/lectures/:lectureId/edit" element={<EditLectureForm />} />
-          <Route path="/announcements" element={<Announcements />} />
-          <Route path="/discussion" element={<GeneralDiscussion />} />
-          <Route path="questions/:questionId" element={<Replies />} />
-        </Route>
-        <Route path="*" element={<h1>Oops, not found!</h1>} />
-      </Routes>
+      <Suspense fallback={<RouteLoadingSpinner />}>
+        <Routes>
+          <Route path='/login' element={<Login />} />
+          <Route path='/Register' element={<Register />} />
+          <Route path="/" element={<ProtectedLayout />}>
+            <Route index element={<Lectures />} />
+            <Route path="/lectures" element={<Lectures />} />
+            <Route path="/lectures/:lectureId" element={<Lecture />} />
+            <Route path="/lectures/new" element={<CreateNewLecture />} />
+            <Route path="/lectures/:lectureId/edit" element={<EditLectureForm />} />
+            <Route path="/announcements" element={<Announcements />} />
+            <Route path="/discussion" element={<GeneralDiscussion />} />
+            <Route path="questions/:questionId" element={<Replies />} />
+          </Route>
+          <Route path="*" element={<h1>Oops, not found!</h1>} />
+        </Routes>
+      </Suspense>
       <Toaster reverseOrder={true} />
     </div>
   );
